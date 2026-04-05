78aaebe7a262a04151ebf398db25e788bded1424
jskyzero <jskyzero@outlook.com>
2026-04-06 02:13:27 +0800

    update css

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,7 +1,7 @@
 <template>
   <footer 
     v-if="$nav.currentPage !== 1" 
-    class="absolute bottom-4 right-4 px-2 py-1 bg-gray-100/50 backdrop-blur-md rounded-md text-[10px] text-gray-500 border border-gray-200/50"
+    class="absolute bottom-6 right-8 px-2 py-1 bg-gray-100/50 backdrop-blur-md rounded-md text-[10px] text-gray-500 border border-gray-200/50"
   >
     {{ $nav.currentPage }} / {{ $nav.total }}
   </footer>
