b01c03cd5cc14894fec77d1358b8162b3b817c2f
jskyzero <jskyzero@users.noreply.github.com>
2026-04-05 17:38:44 +0000

    deploy: 0e0bcea1531262850e1062472b2c1cfa7e0bcd57

---
+++ assets/md-Bpx-Hxl4.js
@@ -0,0 +1,13 @@
+import{o as u,b as a,w as l,j as o,g as t,ad as p,v as m,x as i,T as n}from"./modules/vue-C1QxWCQP.js";import{I as d}from"./slidev/default-BI49anp-.js";import{u as w,f}from"./slidev/context-CLeXH2Bn.js";import"./index-BZxrjmG-.js";import"./modules/shiki-B8sKz4aH.js";const k={__name:"slides.md__slidev_11",setup(c){const{$clicksContext:r,$frontmatter:s}=w();return r.setup(),(b,e)=>(u(),a(d,m(i(n(f)(n(s),10))),{default:l(()=>[o(` ### 2014-骷髅女孩
+
+如何在2D游戏中获得最清晰、最流畅的动画并使其与游戏玩法配合。
+
++ 🚧待施工
+
++ 相关参考：[Youtube](https://www.youtube.com/watch?v=bqCSzO15VME) `),o(` ### 2015-骷髅女孩
+
+2D传统动画演示，Skullgirls的动画制作方式。
+
++ 🚧待施工
+
++ 相关参考：[Youtube](https://www.youtube.com/watch?v=z-5djm1pRpU) `),e[0]||(e[0]=t("h3",null,"2015 - 罪恶装备 Xrd",-1)),e[1]||(e[1]=t("p",null,"在现代全3D图形框架内重建经典2D格斗游戏，同时保留其所有老式2D魅力。",-1)),e[2]||(e[2]=t("ul",null,[t("li",null,[t("p",null,"🚧待施工")]),t("li",null,[t("p",null,[p("相关参考："),t("a",{href:"https://www.youtube.com/watch?v=yhGjCzxJV3E",target:"_blank"},"Youtube")])])],-1))]),_:1},16))}};export{k as default};
